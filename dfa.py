c01c171e7326fd6c000bc93c7edbdc0fd1c89914
Dave Rosoff <drosoff@collegeofidaho.edu>
2023-10-02 17:41:24 -0600

    initial commit

---
+++ dfa.py
@@ -0,0 +1,28 @@
+###########################################################################
+# This is a skeleton for HW 03. You can use these classes as is, or
+# change the class structure, but the classes DFA and NFA must have the
+# methods listed for parts A and B. You will need to supply the right
+# parameters for the included methods, and supply your own definitions
+# for the methods in parts C and D.
+#
+# I have seen that some people will try to make classes for everything:
+# states, transitions, etc. This is not necessary. You can do this
+# assignment using just the name of the state (as a string) to represent
+# it.
+###########################################################################
+
+class DFA:
+    def __init__(self,):
+        pass
+
+    def __repr__(self,):
+        pass
+
+    def do_delta(self,):
+        pass
+
+    def run(self,):
+        pass
+
+class NFA:
+    pass
